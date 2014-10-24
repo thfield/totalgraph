4cfe1138b9a23d70d40abca67212ab9c0a70645d
thfield <thfield@gmail.com>
2014-10-24 12:39:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Totalgraph::Application.config.secret_key_base = '1d22f850d58ec9e202fcd08247d2ed37ec736426011c4809a2bd841d4a2be55f5f1d57e9a4cc79f26ca60bd37379c79e6f94491f760c9bb921dc7795df8a62ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Totalgraph::Application.config.session_store :cookie_store, key: '_totalgraph_session'
